0fc13c7ba413eaece5b0a95331066fff4ebe738a
Steve Gomez <StevenAGomez@Live.com>
2024-01-16 11:09:11 -0800

    Adds fast-xml-parser

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ts-node-18-express
+# fast-xml-parser
+
+https://www.npmjs.com/package/fast-xml-parser
+
+Input:
+
+```xml
+<SOAP-ENV:Body xmlns:m = "http://www.xyz.org/quotation">
+  <m:GetQuotationResponse>
+    <m:Quotation>Here is the quotation</m:Quotation>
+  </m:GetQuotationResponse>
+</SOAP-ENV:Body>
+```
+
+Output:
+
+```json
+{
+  "result": {
+    "SOAP-ENV:Body": {
+      "m:GetQuotationResponse": {
+        "m:Quotation": "Here is the quotation"
+      }
+    }
+  }
+}
+```
+
+Related:
+
+- https://github.com/scarabaeus/xml2js-test
+- https://github.com/scarabaeus/xml-js-test
+- https://github.com/scarabaeus/fast-xml-parser-test
+
+Download Stats:
+
+- https://npmtrends.com/fast-xml-parser-vs-xml-js-vs-xml2js
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import { XMLParser, XMLBuilder, XMLValidator } from 'fast-xml-parser';
+
 const app = express();
 
+// const xml = `<?xml version="1.0" encoding="utf-8"?>
+//   <note importance="high" logged="true">
+//       <title>Happy</title>
+//       <todo>Work</todo>
+//       <todo>Play</todo>
+//   </note>`;
+
+const xml = `<SOAP-ENV:Body xmlns:m = "http://www.xyz.org/quotation">
+<m:GetQuotationResponse>
+   <m:Quotation>Here is the quotation</m:Quotation>
+</m:GetQuotationResponse>
+</SOAP-ENV:Body>`;
+
 app.get('/', function (req, res) {
-  res.send('ts-node-18-express');
+  const parser = new XMLParser();
+  const result = parser.parse(xml);
+
+  res.json({
+    result: result,
+  });
 });
 
-app.listen(3000);
+app.listen(8088);
